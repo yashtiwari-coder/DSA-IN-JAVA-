5ffcc7934fcdb6f21c3e45e717a6ee0c58d58bab
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-09 19:34:51 +0530

    Added first_class Java program

--- src/first_class.java
+++ src/first_class.java
@@ -1,13 +1,19 @@
+import java .util.Scanner;
 public class first_class {
+public  static void  main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int rollNumber=sc.nextInt();
+       sc.nextLine();
+    String name = sc.nextLine();
+    String address=sc.nextLine();
+
+    System.out.println("Roll Number is " +rollNumber );
+    System.out.println("My Name is " +name);
+    System.out.println("My Address is " +address);
 
 
 
-
-
-0
-
-
-
+}
 
 
 }
