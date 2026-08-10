b8b7bb629a92efdceb50db8e29daf1701a41ea2b
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-10 22:15:51 +0530

    Added second class code

--- src/second_class.java
+++ src/second_class.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class second_class {
+public static void main(String[] args ){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number ");
+    int n = sc.nextInt();
+
+    int count=0;
+    int sum=0;
+    int rev=0;
+    while(n!=0){
+        int digit = n%10;
+        sum=sum+digit;
+        rev=rev*10+digit;
+        n=n/10;
+        count ++;
+    }
+       System.out.println("total number " +count);
+    System.out.println("the sum of " +sum);
+    System.out.println("in reverse " +rev);
+
+
+
+
+
+
+}
 }
