9f51db6f9655f7183feca3d4c9b1886850b5c0d1
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-10 22:27:50 +0530

    Added palindorme Java program

---
+++ src/palindrome.java
@@ -0,0 +1,50 @@
+
+import java.util.Scanner;
+
+
+public class palindrome {
+    public static void main(String [] args){
+Scanner sc = new Scanner( System.in);
+        System.out.println("Enter the number ");
+int num = sc.nextInt();
+ int original=num;
+ int rev=0;
+ while(num!=0){
+     int digit = num %10;
+     rev =rev*10+digit;
+     num=num/10;
+
+
+
+ }
+        if(original==rev){
+            System.out.println("palindrome");
+        }
+        else{
+            System.out.println("not palindrome");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
