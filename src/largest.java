ae7985a5da07857dc8ff5161e14c80ab755e8a53
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-16 23:58:56 +0530

    Added largest Java program

---
+++ src/largest.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class largest {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[arr.length - 1]);
+    }
+}
