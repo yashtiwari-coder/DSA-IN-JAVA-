0935f1a4388de90754e81e3739cca76c955b821c
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-18 13:55:19 +0530

    Added  stt Java program

---
+++ src/stt.java
@@ -0,0 +1,12 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class stt{
+        public static void main(String[] args ){
+            String str = " PROGRAMMING ";
+           String S1= str.substring(2);
+            System.out.println(S1);
+
+
+
+        }
+}
