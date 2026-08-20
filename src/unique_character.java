a4a12cfb3058aaf8a248877db84fab185529d8ec
yashtiwari-coder <yashsharma78198@gmail.com>
2026-08-20 16:43:50 +0530

    Added  unique_character Java program

---
+++ src/unique_character.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class unique_character {
+    public static void main(String[] args) {
+        String str = "programming";
+        int[] ar = new int[128];
+        for (char ch : str.toCharArray()) {
+            ar[ch]++;
+        }
+        for (char ch : str.toCharArray()) {
+            if (ar[ch] == 1) {
+                System.out.println(ch + " " + ar[ch]);
+                ar[ch] = 0;
+                break;
+            }
+        }
+    }
+}
